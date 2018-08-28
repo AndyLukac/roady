da3973cf4eef82ddc2141f03d83b61f5b5041078
Andrew Lukac <a.lukac28@gmail.com>
2018-08-28 16:24:52 +0200

    started the journey show page

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,5 +1,9 @@
 class Journey < ApplicationRecord
   belongs_to :user
+  has_many :accommodations
+  has_many :flights
+  has_many :trains
+  has_many :cars
 
   validates :name, presence: true
   validates :date, presence: true
