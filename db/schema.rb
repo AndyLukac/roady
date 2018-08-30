54dcd510105cefce599c148bd38a5eba1c05e398
Nayeli Hamzaoui <hamzaoui.sbn@gmail.com>
2018-08-30 18:10:01 +0200

    adding double quotes to the sign in form code

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_08_29_133854) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,13 +52,8 @@ ActiveRecord::Schema.define(version: 2018_08_29_133854) do
     t.string "name"
     t.bigint "user_id"
     t.string "photo"
-<<<<<<< HEAD
-    t.string "start_date"
-    t.string "end_date"
-=======
     t.date "start_date"
     t.date "end_date"
->>>>>>> master
     t.index ["user_id"], name: "index_journeys_on_user_id"
   end
 
