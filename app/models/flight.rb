65f840e8409f8cdcb37dbc2a4234eabeb6477b50
jpheos <jooo.blanchard@gmail.com>
2018-09-07 17:23:02 +0200

    hotfix

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -16,11 +16,11 @@ class Flight < JourneyItem
   def fake_date_to_demoday
     self.departure_date = "2018-10-01 21:35:00".to_datetime if "2018-04-22 23:35:00 +0000".to_datetime == departure_date
     self.departure_date = "2018-10-20 20:00:00".to_datetime if "2018-05-12 22:00:00 +0000".to_datetime == departure_date
-    self.departure_date = "2018-10-21 07:00:00".to_datetime if "2018-05-13 09:00:00 +0000".to_datetime == departure_date
+    self.departure_date = "2018-10-21 07:00:00".to_datetime if "2018-05-12 09:00:00 +0000".to_datetime == departure_date
 
     self.arrival_date = "2018-10-02 10:35:00".to_datetime if "2018-04-23 12:35:00 +0000".to_datetime == arrival_date
     self.arrival_date = "2018-10-21 05:30:00".to_datetime if "2018-05-13 07:30:00 +0000".to_datetime == arrival_date
-    self.arrival_date = "2018-10-21 09:20:00".to_datetime if "2018-05-13 11:20:00 +0000".to_datetime == arrival_date
+    self.arrival_date = "2018-10-21 09:20:00".to_datetime if "2018-05-12 11:20:00 +0000".to_datetime == arrival_date
   end
 
 end
