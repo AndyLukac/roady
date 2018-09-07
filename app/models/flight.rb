b3ca1cd123af2bcaa79f89a15e976c5bc4ec845a
jpheos <jooo.blanchard@gmail.com>
2018-09-07 17:11:39 +0200

    fix fake_date_to_demoday method

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -14,17 +14,13 @@ class Flight < JourneyItem
   private
 
   def fake_date_to_demoday
-    case departure_date
-    when "2018-04-22 23:35:00 +0000".to_datetime then self.departure_date = "2018-10-01 21:35:00".to_datetime
-    when "2018-05-12 22:00:00 +0000".to_datetime then self.departure_date = "2018-10-20 20:00:00".to_datetime
-    when "2018-05-13 09:00:00 +0000".to_datetime then self.departure_date = "2018-10-21 07:00:00".to_datetime
-    end
+    self.departure_date = "2018-10-01 21:35:00".to_datetime if "2018-04-22 23:35:00 +0000".to_datetime == departure_date
+    self.departure_date = "2018-10-20 20:00:00".to_datetime if "2018-05-12 22:00:00 +0000".to_datetime == departure_date
+    self.departure_date = "2018-10-21 07:00:00".to_datetime if "2018-05-13 09:00:00 +0000".to_datetime == departure_date
 
-    case arrival_date
-    when "2018-04-23 12:35:00 +0000".to_datetime then self.arrival_date = "2018-10-02 10:35:00".to_datetime
-    when "2018-05-13 07:30:00 +0000".to_datetime then self.arrival_date = "2018-10-21 05:30:00".to_datetime
-    when "2018-05-13 11:20:00 +0000".to_datetime then self.arrival_date = "2018-10-21 09:20:00".to_datetime
-    end
+    self.arrival_date = "2018-10-02 10:35:00".to_datetime if "2018-04-23 12:35:00 +0000".to_datetime == arrival_date
+    self.arrival_date = "2018-10-21 05:30:00".to_datetime if "2018-05-13 07:30:00 +0000".to_datetime == arrival_date
+    self.arrival_date = "2018-10-21 09:20:00".to_datetime if "2018-05-13 11:20:00 +0000".to_datetime == arrival_date
   end
 
 end
