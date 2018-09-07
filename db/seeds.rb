8a19ac4200b1239481bbb9a339bac72be12734d7
Andrew Lukac <a.lukac28@gmail.com>
2018-09-07 11:37:02 +0200

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,88 +15,115 @@ nayeli = User.create!(
   password: "roady2018"
 )
 
-puts "Je crée Maurice"
-maurice = Journey.create!(
-  name: "Maurice",
-  start_date: Date.parse("09/09/2018"),
-  end_date: Date.parse("18/09/2018"),
-  photo: "https://images.unsplash.com/photo-1535515505335-9319ebc5c0f9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4ec0036447bd2866efc7125ecfa3494c&auto=format&fit=crop&w=2100&q=80",
+puts "Je crée Au pays des merveilles"
+merveilles = Journey.create!(
+  name: "Au pays des merveilles",
+  start_date: Date.parse("20/12/2018"),
+  end_date: Date.parse("26/12/2018"),
+  photo: "https://booknode.com/actus-litteraires/wp-content/uploads/2016/07/alice-au-pays-des-merveilles-fleurs.jpg",
   user: nayeli,
 )
 
-maurice.remote_photo_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgCa1ae0KP-YCUlguGbhb-_i-r961zHujitcs3zQ0crp9UzsNK"
-maurice.save
+merveilles.remote_photo_url = "https://booknode.com/actus-litteraires/wp-content/uploads/2016/07/alice-au-pays-des-merveilles-fleurs.jpg"
+merveilles.save
 
 
-Train.create!(
-  reservation_number: "H1N1",
-  departure_place: "Lyon Part-Dieu",
-  arrival_place: "Paris Charles de Gaulle",
-  departure_date: DateTime.parse("09/09/2018 06:00pm"),
-  arrival_date: DateTime.parse("09/09/2018 08:00pm"),
-  price: 45,
-  journey: maurice
-  )
+puts "Je crée Ireland"
+ireland = Journey.create!(
+  name: "Ireland",
+  start_date: Date.parse("12/03/2019"),
+  end_date: Date.parse("20/03/2019"),
+  photo: "https://www.worldtravelguide.net/wp-content/uploads/2017/04/shu-Ireland-Clare-DoonagoreCastle-244049320-Patryk-Kosmider-copy.jpg",
+  user: nayeli,
+)
+
+ireland.remote_photo_url = "https://www.worldtravelguide.net/wp-content/uploads/2017/04/shu-Ireland-Clare-DoonagoreCastle-244049320-Patryk-Kosmider-copy.jpg"
+ireland.save
+
+puts "Je crée New Zealand"
+newzealand = Journey.create!(
+  name: "New Zealand",
+  start_date: Date.parse("29/06/2019"),
+  end_date: Date.parse("15/07/2019"),
+  photo: "http://www.woolsnz.com/images/Lake%20Hawea.jpg",
+  user: nayeli,
+)
+
+newzealand.remote_photo_url = "http://www.woolsnz.com/images/Lake%20Hawea.jpg"
+newzealand.save
+
+puts "Je crée Vietnam"
+vietnam = Journey.create!(
+  name: "Vietnam",
+  start_date: Date.parse("11/11/2019"),
+  end_date: Date.parse("24/11/2019"),
+  photo: "http://sumabeachlifestyle.com/wp-content/uploads/2016/05/Rice-Fields-in-Sapa-Vietnam.jpg",
+  user: nayeli,
+)
+
+vietnam.remote_photo_url = "http://sumabeachlifestyle.com/wp-content/uploads/2016/05/Rice-Fields-in-Sapa-Vietnam.jpg"
+vietnam.save
 
 Flight.create!(
-  reservation_number: "R2D2",
-  flight_number: "AN 228",
-  departure_place: "Paris Charles de Gaulle",
-  arrival_place: "Mahébourg, île Maurice",
-  departure_date: DateTime.parse("09/09/2018 10:00pm"),
-  arrival_date: DateTime.parse("10/09/2018 12:00pm"),
-  price: 816,
-  journey: maurice
-  )
-
-Car.create!(
-  reservation_number: "H1N1",
-  departure_place: "Plaine Magnien-Mahebourg Road, Maurice",
-  arrival_place: "Plaine Magnien-Mahebourg Road, Maurice",
-  brand: "Toyota",
-  departure_date: DateTime.parse("10/09/2018 02:00pm"),
-  arrival_date: DateTime.parse("17/09/2018 09:00pm"),
-  hiring_days: 12,
-  price: 485,
-  journey: maurice
-  )
-
+  reservation_number: "G16THY",
+  flight_number: "H1 YT56",
+  departure_place: "Lyon, France",
+  arrival_place: "Phú Minh, Sóc Sơn, Hà Nội, Vietnam",
+  departure_date: DateTime.parse("11/11/2018 06:00pm"),
+  arrival_date: DateTime.parse("12/11/2018 03:00am"),
+  price: 2005,
+  journey: vietnam
+)
 
 Accommodation.create!(
   reservation_number: "RESA1",
-  arrival_date: DateTime.parse("10/09/2018 05:00pm"),
-  departure_date: DateTime.parse("14/09/2018 10:00am"),
-  name: "Stunning cabin on the sea",
-  owner: "Juan Andreas",
-  address: "Village Hall Street, Grande Rivière Sud Est, Maurice",
-  guests: 1,
-  image: "https://images.unsplash.com/photo-1524082983062-21c24967d6c9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0a1532b84f66efdf63f7af7b1f0493f5&auto=format&fit=crop&w=858&q=80",
+  arrival_date: DateTime.parse("12/11/2018 07:00am"),
+  departure_date: DateTime.parse("17/11/2018 10:00am"),
+  name: "Comfy Studio Apt near Thien Quang Lake",
+  owner: "JHai Bà Trưng",
+  address: "27 Đồng Nhân, Hai Bà Trưng, Hà Nội, Vietnam",
+  guests: 2,
+  image: "https://a0.muscache.com/im/pictures/e8425ed4-612a-4abf-815f-ea88464ef0ad.jpg?aki_policy=xx_large",
   price: 318,
-  telephone: 5000,
-  journey: maurice
-  )
-
-
-Flight.create!(
-  reservation_number: "4242",
-  flight_number: "AN 42",
-  departure_place: "Mahébourg, île Maurice",
-  arrival_place: "Paris Charles de Gaulle",
-  departure_date: DateTime.parse("17/09/2018 11:00pm"),
-  arrival_date: DateTime.parse("18/09/2018 11:30am"),
-  price: 712,
-  journey: maurice
+  telephone: 347383683,
+  journey: vietnam
   )
 
 Train.create!(
-  reservation_number: "G42D",
-  departure_place: "Paris Charles de Gaulle",
-  arrival_place: "Lyon Part-Dieu",
-  departure_date: DateTime.parse("18/09/2018 02:30pm"),
-  arrival_date: DateTime.parse("18/09/2018 04:30pm"),
+  reservation_number: "T16THY",
+  departure_place: "120 Lê Duẩn, Văn Miếu, Hoàn Kiếm, Hà Nội, Vietnam",
+  arrival_place: "75 Lương Khánh Thiện, Lương Khá Thien, Ngô Quyền, Hải Phòng, Vietnam",
+  departure_date: DateTime.parse("17/11/2018 12:00pm"),
+  arrival_date: DateTime.parse("17/11/2018 03:00pm"),
   price: 45,
-  journey: maurice
+  journey: vietnam
+)
+
+Accommodation.create!(
+  reservation_number: "HGTYF21",
+  arrival_date: DateTime.parse("17/11/2018 10:00am"),
+  departure_date: DateTime.parse("23/11/2018 02:00pm"),
+  name: "KIARA’S HOME - UNIQUE VILLA. Room 1",
+  owner: "Đằng Hải",
+  address: "89 Đằng Hải, Hải An, Hải Phòng, Vietnam",
+  guests: 2,
+  image: "https://a0.muscache.com/im/pictures/6d741345-a4a5-4976-a8aa-6c67d0877759.jpg?aki_policy=xx_large",
+  price: 318,
+  telephone: 347383683,
+  journey: vietnam
   )
+
+Flight.create!(
+  reservation_number: "HN568459",
+  flight_number: "AN 42",
+  departure_place: "Lê Hồng Phong, Hải An, Đằng Lâm Hải An Hải Phòng, Vietnam",
+  arrival_place: "Lyon, France",
+  departure_date: DateTime.parse("23/11/2018 05:00pm"),
+  arrival_date: DateTime.parse("24/11/2018 04:30am"),
+  price: 712,
+  journey: vietnam
+  )
+
 
 p JourneyItem.all
 
